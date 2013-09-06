4fb6edcf5d3e040f817fda9ca8bff7cc60e4ee4f
Dan Esparza <esparza.dan@gmail.com>
2013-09-06 17:15:23 -0400

    Added SendPush console utility

---
+++ SendPush/Options.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+
+namespace SendPush
+{
+    public class Options
+    {
+        [Option('t', "title", HelpText = "The message title")]
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        [Option('m', "message", HelpText = "The message")]
+        public string Message
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The Pushover.NET api key to send the message from
+        /// </summary>
+        [Option('f', "from", HelpText = "The Pushover api key to send the message from")]
+        public string From
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The Pushover.NET api key to send the message to
+        /// </summary>
+        [Option('u', "user", HelpText = "The Pushover api key to send the message to")]
+        public string User
+        {
+            get;
+            set;
+        }
+
+        [HelpOption('?', "help", HelpText = "Show this help screen")]
+        public string GetUsage()
+        {
+            var help = new HelpText
+            {
+                Heading = new HeadingInfo("SendPush", "0.1"),
+                Copyright = new CopyrightInfo("Dan Esparza", 2013),
+                AddDashesToOption = true
+            };
+
+            help.AddOptions(this);
+
+            return help;
+        }
+    }
+}
---
+++ SendPush/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PushoverClient;
+
+namespace SendPush
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Get the settings defaults
+            string appKey = ConfigurationManager.AppSettings["appKey"];
+            string userGroupKey = ConfigurationManager.AppSettings["userGroupKey"];
+
+            //  Get the command line options
+            Options options = new Options();
+            if(CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                //  If we didn't get the app key passed in, use the default:
+                if(string.IsNullOrEmpty(options.From))
+                {
+                    options.From = appKey;
+                }
+
+                //  If we didn't get the user key passed in, use the default:
+                if(string.IsNullOrEmpty(options.User))
+                {
+                    options.User = userGroupKey;
+                }
+
+                //  Send the message
+                Pushover pclient = new Pushover(options.From);
+                PushResponse response = pclient.Push(options.Title, options.Message, options.User);
+            }
+        }
+    }
+}
